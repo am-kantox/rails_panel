65a9e5dd50bd5c16813eef9f8805d7f044b92a23
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2015-04-14 18:30:35 +0200

    Fixed icons.

--- rails_panel/assets/javascripts/devtools.js
+++ rails_panel/assets/javascripts/devtools.js
@@ -1,1 +1,7 @@
 chrome.devtools.panels.create("Rails", "assets/images/toolbarIcon.png", "panel.html");
+
+//chrome.devtools.panels.elements.createSidebarPane("Rails Monitor",
+//    function(sidebar) {
+//        // sidebar initialization code here
+//        sidebar.setObject({ some_data: "Some data to show" });
+//});
